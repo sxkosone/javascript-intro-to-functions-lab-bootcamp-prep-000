5d305044fff9b94e6032706f5f74e126d8d79577
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-08 18:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
  function sayHiToGrandma(string) {
-   if (string.toLowerCase === string) {
+   if (string.toLowerCase() === string) {
      return 'I can\'t hear you!';
-   } else if (string.toUpperCase === string) {
+   } else if (string.toUpperCase() === string) {
      return 'YES INDEED!';
    } else if (string === "I love you, Grandma.") {
      return "I love you, too.";
